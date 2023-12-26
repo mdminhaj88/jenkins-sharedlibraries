2dc46b3257f232725f883a9b82f923607d55c86a
mdminhaj88 <142885612+mdminhaj88@users.noreply.github.com>
2023-12-26 20:02:56 +0530

    Update kubernetesHelmDeploy.groovy

--- vars/kubernetesHelmDeploy.groovy
+++ vars/kubernetesHelmDeploy.groovy
@@ -13,7 +13,7 @@ def call (String dockerRegistry, String dockerImageTag, String helmChartName, St
      aws eks --region ap-south-1 update-kubeconfig --name eks-cluster
     """
     sh """ 
-    helm upgrade --install $helmChartName ./helm --set image.repository="$dockerRegistry:$dockerImageTag"
+    helm upgrade --install $helmChartName ./helm/nodejs-useraccount --set image.repository="$dockerRegistry:$dockerImageTag"
     """
    
 }
